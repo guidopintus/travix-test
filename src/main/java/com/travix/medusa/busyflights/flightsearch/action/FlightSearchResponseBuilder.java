6a34e692218558c0c16c63e620d66b9da062767a
Guido Pintus <g.pintus@vargroup.it>
2019-06-16 20:30:15 +0200

    flight ordering on response builder

--- src/main/java/com/travix/medusa/busyflights/flightsearch/action/FlightSearchResponseBuilder.java
+++ src/main/java/com/travix/medusa/busyflights/flightsearch/action/FlightSearchResponseBuilder.java
@@ -4,7 +4,9 @@ import com.travix.medusa.busyflights.domain.busyflights.BusyFlightsResponse;
 import com.travix.medusa.busyflights.flightsearch.model.FlightInfoElement;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FlightSearchResponseBuilder implements ResponseBuilder {
@@ -16,7 +18,10 @@ public class FlightSearchResponseBuilder implements ResponseBuilder {
     }
 
     public FlightSearchResponseBuilder addFlights(List<FlightInfoElement> infoElements) {
-        this.infoElements=infoElements;
+        this.infoElements=infoElements
+                .stream()
+                .sorted(Comparator.comparingDouble(FlightInfoElement::getFare))
+                .collect(Collectors.toList());
         return this;
     }
 
